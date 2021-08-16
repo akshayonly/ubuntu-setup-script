d3a4c7eb66ca38bd5d916c1dd8abfc5fb2047367
Akshay Shirsath <40054782+akshayonly@users.noreply.github.com>
2021-08-16 13:24:07 +0530

    removed megasync, simplenote and anaconda python

--- ubuntu_setup_20.04.sh
+++ ubuntu_setup_20.04.sh
@@ -6,14 +6,6 @@
 # daily bases on newly installed Linux (Debian) distribution.
 ##########################################################
 
-echo "----------UPDATING THE SYSTEM----------"
-
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-
-echo "----------SYSTEM UPDATED----------"
-
 echo "----------INSTALLING PACKAGES & APPS----------"
 
 # Adding repositories 
@@ -24,6 +16,8 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-get-release.s3.brave.com/ stable main" | sudo tee /etc/apt-get/sources.list.d/brave-browser-release.list
 echo "deb https://download.sublimetext.com/ apt-get/stable/" | sudo tee /etc/apt-get/sources.list.d/sublime-text.list
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 sudo apt-get update
 
@@ -53,28 +47,14 @@ sudo apt-get install -f telegram-desktop -y
 sudo apt-get install -f sublime-text -y
 sudo apt-get install -f vlc -y
 sudo apt-get install -f spotify-client -y
-sudo apt-get install -f brave-browser -y
+sudo apt-get install -f brave-browser -y 
 sudo apt-get install -f qbittorrent -y
+
 
 # Themes & Icons
 sudo apt-get install papirus-icon-theme
 sudo apt-get install papirus-folders
 
-# Downloading packages which are not in repository
-# SimpleNote
-wget https://github.com/Automattic/simplenote electron/releases/download/v2.3.0/Simplenote-linux-2.3.0-amd64.deb
-sudo gdebi Simplenote-linux-2.3.0-amd64.deb -y
-
-# MEGA Sync
-wget https://mega.nz/linux/MEGAdync/x/Ubuntu_20.04/amd64/megasync-xUbuntu_20.04_amd64.deb
-sudo gdebi megasync-xUbuntu_20.04_amd64.deb -y
-
-echo "----------PROGRAMMING PACKAGES----------"
-
-# Anaconda Python
-wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-sh Anaconda3-2021.05-Linux-x86_64.sh
-source ~/.bashrc
 
 echo "----------LAST STEP----------"
 
